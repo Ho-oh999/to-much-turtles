0f44cada5829de5cc00d6fafc2f783e78c8c4ea2
boko999 <ho-oh999@hotmail.com>
2020-07-23 17:17:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import turtle
+
+turtle.title("My Turtle Game")
+turtle.bgcolor("blue")
+turtle.setup(600,600)
+screen = turtle.Screen()
+bob = turtle.Turtle()
+bob.shape("turtle")
+
+
+
+
+
+
+
+def star(length, red):
+  bob.fillcolor(red)
+  bob.begin_fill()
+  x=0
+  while x <15:
+    bob.forward(int(length))
+    bob.right(144)
+    x += 1
+  bob.end_fill()
